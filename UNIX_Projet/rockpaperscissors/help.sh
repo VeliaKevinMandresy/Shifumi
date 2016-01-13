78c0c9e2db55aa91db503de7a2deea5341013717
VELIA Kevin Mandresy <velia_k@etna-alternance.net>
2016-01-13 23:57:10 +0100

    Modification

--- UNIX_Projet/rockpaperscissors/help.sh
+++ UNIX_Projet/rockpaperscissors/help.sh
@@ -1,8 +1,8 @@
 help(){
-	echo -e \\033[33\;1m"Run script without options to get this help;
-Run with option \"a\" for [a]utomated (CPU vs CPU) game;
-Run with option \"h\" for [h]uman (Player vs CPU) game."\\033[0m
-	echo "Rules: "
+	#echo -e \\033[33\;1m"Run script without options to get this help;
+#Run with option \"a\" for [a]utomated (CPU vs CPU) game;
+#Run with option \"h\" for [h]uman (Player vs CPU) game."\\033[0m
+	echo "Rules: \n"
 	echo -e "rock crushes scissors\nscissors cut paper\npaper covers rock"
 	echo -e \\033[33\;1m"Table usage: Result of\"row\" to \"column\" draw
 0: draw / 1: \"row\" loses to \"column\" / 2: \"row\" wins to \"column\"
--- UNIX_Projet/rockpaperscissors/shifumi.sh
+++ UNIX_Projet/rockpaperscissors/shifumi.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 clear
-victoire(){
-	Achoice=$[($RANDOM % ($[3 - 1] + 1)) + 1] 
-	if [[ $Choice == $Achoice ]]; then
-		echo -e "\n> Equality"		
-	elif [[ $Choice == "1" && $Achoice == "2" ]]; then
-		echo -e "\n> Paper covers Rock"		
-	elif [[ $Choice == "1" && $Achoice == "3" ]]; then
-		echo -e "\n> Rock crushes Scissors"
-		Score=$[$Score+1]
-	elif [[ $Choice == "2" && $Achoice == "1" ]]; then
-		echo -e "\n> Paper covers Rock"
-		Score=$[$Score+1]
-	elif [[ $Choice == "2" && $Achoice == "3" ]]; then
-		echo -e "\n> Scissors cut Paper"
-	elif [[ $Choice == "3" && $Achoice == "1" ]]; then
-		echo -e "\n> Rock crushes Scissors"
-	elif [[ $Choice == "3" && $Achoice == "2" ]]; then
-		echo -e "\n> Scissors cut Paper"
-		Score=$[$Score+1]	
-	fi
-	echo -e "> Score: " $Score "\n"
-}
-
 if [[ $1 > "1" ]] && [[ $1 -le "10" ]]
 then
 	case $Choice in
