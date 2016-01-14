a50eb74b6edb7dcef493e70398ff89ce274934bc
VeliaKevinMandresy <velia_k@etna-alternance.net>
2016-01-14 16:25:32 +0100

    Modification rockpaperscissors

--- UNIX_Projet/rockpaperscissors/fonctions/result.sh
+++ UNIX_Projet/rockpaperscissors/fonctions/result.sh
@@ -1,23 +1,31 @@
-result(){
-	
-	Achoice=$[($RANDOM % ($[3 - 1] + 1)) + 1] 
-	if [[ $Choice == $Achoice ]]; then
-		echo -e "\n> Equality"		
-	elif [[ $Choice == "1" && $Achoice == "2" ]]; then
-		echo -e "\n> Paper covers Rock"		
-	elif [[ $Choice == "1" && $Achoice == "3" ]]; then
-		echo -e "\n> Rock crushes Scissors"
-		Score=$[$Score+1]
-	elif [[ $Choice == "2" && $Achoice == "1" ]]; then
+loose(){
+	Achoice=$[($RANDOM % ($[3 - 1] + 1)) + 1]
+	if [[ $Choice == "1" && $Achoice == "2" ]]; then
 		echo -e "\n> Paper covers Rock"
-		Score=$[$Score+1]
 	elif [[ $Choice == "2" && $Achoice == "3" ]]; then
 		echo -e "\n> Scissors cut Paper"
 	elif [[ $Choice == "3" && $Achoice == "1" ]]; then
 		echo -e "\n> Rock crushes Scissors"
+	fi	 
+}
+
+equality(){
+	Achoice=$[($RANDOM % ($[3 - 1] + 1)) + 1] 
+	if [[ $Choice == $Achoice ]]; then
+		echo -e "\n> Equality"
+	fi	
+}
+
+win(){
+	Achoice=$[($RANDOM % ($[3 - 1] + 1)) + 1]
+	if [[ $Choice == "1" && $Achoice == "3" ]]; then
+		echo -e "\n> Rock crushes Scissors"
+		Score=$[$Score+1]
+	elif [[ $Choice == "2" && $Achoice == "1" ]]; then
+			echo -e "\n> Paper covers Rock"
+			Score=$[$Score+1]
 	elif [[ $Choice == "3" && $Achoice == "2" ]]; then
-		echo -e "\n> Scissors cut Paper"
-		Score=$[$Score+1]	
+			echo -e "\n> Scissors cut Paper"
+			Score=$[$Score+1]	
 	fi
-	echo -e "> Score: " $Score "\n"
 }
--- UNIX_Projet/rockpaperscissors/shifumi_with_bonus.sh
+++ UNIX_Projet/rockpaperscissors/shifumi_with_bonus.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 clear
 source fonctions/result.sh
+source fonctions/affichage.sh
+source fonctions/help.sh
 # Call functions 
+affichage
+help
 if [[ $1 > "1" ]] && [[ $1 -le "10" ]]
 then
 	case $Choice in
 		1 ) $Choice = "Rock" ;;
 		2 ) $Choice = "Paper" ;;
 		3 ) $Choice = "Scissors" ;;
+		4 ) $Choice = "Lezard" ;;
+		5 ) $Choice = "Spoke" ;;
 	esac
-echo -e "Ready to play Shifumi? Here we go! \n"
+echo -e "Ready to play Shifumi? Here we go!\n"
         Score="0"
 		while [[ $Score != $1 ]]
 		do
 			echo -e "      1- rock"
 			echo -e "      2- paper"
 			echo -e "      3- scissors"
-			echo -e "      X- Quit game \n"
+			echo -e "      X- Quit games\n"
 			read -p "Make choice: " Choice
 			if [[ $Choice == X ]]; then
 				Score=$1 
@@ -35,14 +41,14 @@ echo -e "Ready to play Shifumi? Here we go! \n"
 					win
 					echo -e "> Score: " $Score "\n";;		
 				X ) echo -e "> Score: " $Score "\n";;
-			  # help) help;;
+			  help) help;;
 				* ) echo -e "\nError: You make bad choice. Try again.\n";;	
 			esac
 		done
-		# echo -e "\n""------------------------ Congratulation ------------------------""\n"
+		echo -e "\n""------------------------ Congratulation ------------------------""\n"
 		echo -e "	      		You won!"
 		echo -e "	      See you on rock-paper-scissors. \n"
-		# echo -e "\n""---------------------------------------------------------------""\n"
+		echo -e "\n""---------------------------------------------------------------""\n"
 elif [[ -z $i ]]; then
  	echo -e "\n Error: Vous n'avez pas entrer les bonnes informations \n" #help option if $1 empty
 fi
