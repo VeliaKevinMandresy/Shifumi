332290a5270157644ba1ab43b3abe5af4b47e0ac
VeliaKevinMandresy <velia_k@etna-alternance.net>
2016-01-14 17:00:31 +0100

    modif fonctions/help

--- UNIX_Projet/rockpaperscissors/fonctions/help.sh
+++ UNIX_Projet/rockpaperscissors/fonctions/help.sh
@@ -2,9 +2,9 @@ help(){
 	#echo -e \\033[33\;1m"Run script without options to get this help;
 #Run with option \"a\" for [a]utomated (CPU vs CPU) game;
 #Run with option \"h\" for [h]uman (Player vs CPU) game."\\033[0m
-	echo -e \\033[33\;1m"Rules: \n"
-	echo -e \\033[33\;1m"rock crushes scissors\nscissors cut paper\npaper covers rock"
-	echo -e \\033[33\;1m"Table usage: Result of\"row\" to \"column\" draw
+	echo -e \\033[36\;1m"\nRules: \n"\\033[0m
+	echo -e ">>> "\\033[32\;1m "rock crushes scissors\n"\\033[0m">>> "\\033[32\;1m"scissors cut paper\n"\\033[0m">>> "\\033[32\;1m"paper covers rock"\\033[0m
+	echo -e \\033[33m"Table usage: Result of\"row\" to \"column\" draw
 0: draw / 1: \"row\" loses to \"column\" / 2: \"row\" wins to \"column\"
 -----------------------------------
         | Rock | Paper | Scissors |
@@ -14,5 +14,5 @@ help(){
   Paper |  2   |   0   |    1     |
 --------|------|-------|----------|
 Scissors|  1   |   2   |    0     |
------------------------------------"\\033[0\;0m
+-----------------------------------"\\033[0m
 }
--- UNIX_Projet/rockpaperscissors/shifumi.sh
+++ UNIX_Projet/rockpaperscissors/shifumi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 clear
 source fonctions/result.sh
-# Call functions 
+source fonctions/affichage.sh
+source fonctions/help.sh
+# Call functions
+affichage
+
 if [[ $1 > "1" ]] && [[ $1 -le "10" ]]
 then
 	case $Choice in
@@ -16,6 +20,7 @@ echo -e "Ready to play Shifumi? Here we go! \n"
 			echo -e "      1- rock"
 			echo -e "      2- paper"
 			echo -e "      3- scissors"
+			echo -e "      help- If you need help"
 			echo -e "      X- Quit game \n"
 			read -p "Make choice: " Choice
 			if [[ $Choice == X ]]; then
@@ -35,7 +40,7 @@ echo -e "Ready to play Shifumi? Here we go! \n"
 					win
 					echo -e "> Score: " $Score "\n";;		
 				X ) echo -e "> Score: " $Score "\n";;
-			  # help) help;;
+			  help) help;;
 				* ) echo -e "\nError: You make bad choice. Try again.\n";;	
 			esac
 		done
@@ -44,5 +49,5 @@ echo -e "Ready to play Shifumi? Here we go! \n"
 		echo -e "	      See you on rock-paper-scissors. \n"
 		# echo -e "\n""---------------------------------------------------------------""\n"
 elif [[ -z $i ]]; then
- 	echo -e "\n Error: Vous n'avez pas entrer les bonnes informations \n" #help option if $1 empty
+ 	echo -e \\033[31\;7m"\n Error:"\\033[0m" Vous n'avez pas entrer les bonnes informations \n" #help option if $1 empty
 fi
